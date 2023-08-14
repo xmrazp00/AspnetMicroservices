1f901432b8cda480bbf2dbfe377a248a0d5bc523
xmrazp00 <xmrazp00>
2023-08-14 22:33:37 +0200

    2.12

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,61 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+using System.Xml.Linq;
+
+namespace Catalog.API.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly ICatalogContext _context;
+
+    public ProductRepository(ICatalogContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Product>> GetProducts()
+    {
+        return await _context.Products.Find(x => true).ToListAsync();
+    }
+
+    public async Task<Product> GetProduct(string id)
+    {
+        return await _context.Products.Find(x => x.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<Product>> GetProductByName(string name)
+    {
+        var filter = Builders<Product>.Filter.Eq(x => x.Name, name);
+
+        return await _context.Products.Find(filter).ToListAsync();
+    }
+
+    public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+    {
+        var filter = Builders<Product>.Filter.Eq(x => x.Category, categoryName);
+
+        return await _context.Products.Find(filter).ToListAsync();
+    }
+
+    public async Task CreateProduct(Product product)
+    {
+        await _context.Products.InsertOneAsync(product);
+    }
+
+    public async Task<bool> UpdateProduct(Product product)
+    {
+        var updateResult = await _context.Products.ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+
+        return updateResult.IsAcknowledged
+            && updateResult.ModifiedCount > 0;
+    }
+
+    public async Task<bool> DeleteProduct(string id)
+    {
+        var deleteResult = await _context.Products.DeleteOneAsync(filter: g => g.Id == id);
+
+        return deleteResult.IsAcknowledged
+               && deleteResult.DeletedCount > 0;
+    }
+}
