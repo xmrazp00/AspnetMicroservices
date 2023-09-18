3b21e2e23aa2e6bd8f576a3b656cf832dbf24782
Patrik Mráz <patrik.mraz0@gmail.com>
2023-09-18 22:30:23 +0200

    fix

--- src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -4,8 +4,7 @@
     {
         public string UserName { get; set; }
         public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
-        public TYPE Type { get; set; }
-        
+
         public ShoppingCart(string userName)
         {
             UserName = userName;
