5a482482de999f251a3c1cfbf2e7965e29798e4d
Patrik Mráz <patrik.mraz0@gmail.com>
2023-11-05 23:31:46 +0100

    5 end

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,5 +1,6 @@
 using Discount.Grpc.Extensions;
 using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddAutoMapper(typeof(Program));
 
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
@@ -15,6 +17,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 //app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.MigrateDatabase<Program>();
