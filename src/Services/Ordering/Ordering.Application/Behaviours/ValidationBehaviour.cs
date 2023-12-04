fcf95e8e8a7d35ef9b04f1a8a99d39b1192d9321
Patrik Mráz <patrik.mraz0@gmail.com>
2023-12-04 22:41:33 +0100

    7.22

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+
+namespace Ordering.Application.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                var validationsResults =
+                    await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationsResults.SelectMany(x => x.Errors).Where(f => f != null).ToList();
+
+                if (failures.Any())
+                {
+                    throw new Application.Exceptions.ValidationException(failures);
+                }
+            }
+
+            return await next();
+        }
+    }
+}
