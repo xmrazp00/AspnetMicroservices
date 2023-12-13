8719ac669635f40f48df0a9f7b28f9f25f0f04a4
Patrik Mráz <patrik.mraz0@gmail.com>
2023-12-13 22:28:08 +0100

    7.26

---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistance;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            return await _dbContext.Orders.Where(x => x.UserName == userName).ToListAsync();
+        }
+    }
+}
